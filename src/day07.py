459889a554a3f90128d346281c90a8950a825669
chris <christoph-stade@outlook.de>
2021-12-07 13:15:25 +0100

    removed unnecessary check

--- src/day07.py
+++ src/day07.py
@@ -5,23 +5,19 @@ l = [int(i) for i in l[0].replace("\n", "").split(",")]
 
 def solve(part1 = False):
     c = {}
-    s = []
 
     for r in range(min(l), max(l)):
-        if r not in s:
-            s.append(r)
+        for k in l:
+            if not c.get(r):
+                c[r] = 0
 
-            for k in l:
-                if not c.get(r):
-                    c[r] = 0
+            n = abs(r - k)
 
-                n = abs(r - k)
+            if part1:
+                c[r] += n
+                continue
 
-                if part1:
-                    c[r] += n
-                    continue
-
-                c[r] += int(n * (n + 1) / 2)
+            c[r] += int(n * (n + 1) / 2)
 
     return c[min(c, key=c.get)]
 
